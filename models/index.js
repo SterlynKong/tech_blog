a780f72de90066b5738b2b547c55001f7fa9dbb6
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-12-07 18:32:48 -0500

    added index.js to bring models together, staged style.css, udated server.js

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
+    foreignKey: 'user_Id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_Id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_Id',
+    onDelete: 'CASCADE'
+});
+
+
+module.exports = {
+    User,
+    Comment,
+    Post
+};
--- server.js
+++ server.js
@@ -15,8 +15,36 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // create instance of handlebars
-const hbs = exphbs.create({});
+const hbs = exphbs.create({
+    helpers: {
+        format_date: date => {
+            return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        }
+    }
+});
+
+// express session
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    })
+};
 
 // register hbs.engine with express app
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.statis(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
+});
